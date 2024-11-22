5d9e4ce7b54c2daa5ec9d5b1a732e83bd66317c6
GIORGIO CAVION TIBURI <04220645055@senacrs.edu.br>
2024-11-22 19:45:53 -0300

    VERSÃO 1.0

---
+++ add-delet-edit/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace add_delet_edit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lsvLista = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBusca = new System.Windows.Forms.TextBox();
+            this.txtDeletar = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(15, 25);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome";
+            // 
+            // lsvLista
+            // 
+            this.lsvLista.HideSelection = false;
+            this.lsvLista.Location = new System.Drawing.Point(121, 9);
+            this.lsvLista.Name = "lsvLista";
+            this.lsvLista.Size = new System.Drawing.Size(360, 143);
+            this.lsvLista.TabIndex = 2;
+            this.lsvLista.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Adicionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(15, 64);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Telefone";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(12, 103);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(118, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Procurar";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(233, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Remover";
+            // 
+            // txtBusca
+            // 
+            this.txtBusca.Location = new System.Drawing.Point(121, 171);
+            this.txtBusca.Name = "txtBusca";
+            this.txtBusca.Size = new System.Drawing.Size(100, 20);
+            this.txtBusca.TabIndex = 10;
+            // 
+            // txtDeletar
+            // 
+            this.txtDeletar.Location = new System.Drawing.Point(236, 171);
+            this.txtDeletar.Name = "txtDeletar";
+            this.txtDeletar.Size = new System.Drawing.Size(100, 20);
+            this.txtDeletar.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(121, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Procurar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(236, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Remover";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtDeletar);
+            this.Controls.Add(this.txtBusca);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lsvLista);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lsvLista;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBusca;
+        private System.Windows.Forms.TextBox txtDeletar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ add-delet-edit/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace add_delet_edit
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection Conecao;
+        private string data_source = "datasource=localhost;port=3306;username=root;password='';database=db_agenda2";
+        public Form1()
+        {
+            InitializeComponent();
+
+            lsvLista.View = View.Details;
+            lsvLista.LabelEdit = true;
+            lsvLista.AllowColumnReorder = true;
+            lsvLista.GridLines = true;
+
+            lsvLista.Columns.Add("ID", 30, HorizontalAlignment.Left);
+            lsvLista.Columns.Add("Nome", 150, HorizontalAlignment.Left);
+            lsvLista.Columns.Add("Email", 150, HorizontalAlignment.Left);
+            lsvLista.Columns.Add("Telefone", 150, HorizontalAlignment.Left);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Criar conexão MySQL
+                Conecao = new MySqlConnection(data_source);
+
+                // Comando SQL para inserção
+                string sql = "INSERT INTO coo (nome, email, telefone) VALUES (@nome, @email, @telefone)";
+                MySqlCommand comando = new MySqlCommand(sql, Conecao);
+
+                comando.Connection = Conecao; // 
+                // Usando parâmetros 
+                comando.Parameters.AddWithValue("@nome", txtNome.Text);
+                comando.Parameters.AddWithValue("@email", txtEmail.Text);
+                comando.Parameters.AddWithValue("@telefone", txtTelefone.Text);
+
+                // Abrir conexão
+                Conecao.Open();
+
+                // Executar comando
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Dados inseridos com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message); // Exibe a mensagem de erro
+            }
+            finally
+            {
+                Conecao.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string q = "%" + txtBusca.Text + "%"; // Adiciona os % para a busca
+
+                // Criar conexão MySQL
+                Conecao = new MySqlConnection(data_source);
+
+                // Comando SQL para buscar os dados
+                string sql = "SELECT * FROM coo WHERE nome LIKE @q OR email LIKE @q OR id LIKE @q";
+                MySqlCommand comando = new MySqlCommand(sql, Conecao);
+
+                // Usando parâmetros 
+                comando.Parameters.AddWithValue("@q", q);
+
+                // Abrir conexão
+                Conecao.Open();
+
+                // Executar comando
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                // Limpar itens do ListView antes de adicionar novos
+                lsvLista.Items.Clear();
+
+                // Preencher o ListView com os resultados
+                while (reader.Read())
+                {
+                    string[] row =
+                    {
+                        reader.GetValue(0).ToString(),// ID
+                        reader.GetString(1),// Nome
+                        reader.GetString(2),//Email
+                        reader.GetValue(3).ToString()// Telefone
+                    };
+
+                    var linha_listview = new ListViewItem(row);
+                    lsvLista.Items.Add(linha_listview);
+                }
+
+                MessageBox.Show("Busca realizada com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message); // Exibe a mensagem de erro
+            }
+            finally
+            {
+                Conecao.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string q = "%" + txtDeletar.Text + "%"; // Adiciona os % para a busca
+
+                // Criar conexão MySQL
+                Conecao = new MySqlConnection(data_source);
+
+                // Comando SQL para buscar os dados
+                string sql = "DELETE FROM contato WHERE id LIKE @q";
+                MySqlCommand comando = new MySqlCommand(sql, Conecao);
+
+                // Usando parâmetros para evitar SQL Injection
+                comando.Parameters.AddWithValue("@q", q);
+
+                // Abrir conexão
+                Conecao.Open();
+
+                // Executar comando
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                // Limpar itens do ListView antes de adicionar novos
+                lsvLista.Items.Clear();
+
+                // Preencher o ListView com os resultados
+                while (reader.Read())
+                {
+                    string[] row =
+                    {
+                        reader.GetValue(0).ToString(),// ID
+                        reader.GetString(1),// Nome
+                        reader.GetString(2),//Email
+                        reader.GetValue(3).ToString()// Telefone
+                };
+
+                    var linha_listview = new ListViewItem(row);
+                    lsvLista.Items.Add(linha_listview);
+                }
+
+                MessageBox.Show("Busca realizada com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message); // Exibe a mensagem de erro
+            }
+            finally
+            {
+                Conecao.Close();
+            }
+        }
+    }
+}
